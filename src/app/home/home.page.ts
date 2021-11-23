a011a7d98437fc4b3f6a6e99b7a44a1a6a3009d6
rania arbash <raniaarbash@hotmail.com>
2021-11-22 19:41:41 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchType, MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  isLoading = false;
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+  }
+
+  searchChanged() {
+    this.isLoading = true;
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+    this.isLoading = false;
+  }
+
+
+
 
 }
---
+++ src/app/movie-details/movie-details/movie-details.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+movieInfo: any;
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieService.
+    getDetails(id).
+    subscribe(data => this.movieInfo = data);
+  }
+
+  openWebsite() {
+    window.open(this.movieInfo.Website, '_blank'); // opens the link in the new tab.
+
+  }
+
+}
